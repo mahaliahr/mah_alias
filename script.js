34452a543341c71aca71a5bf385891ee87358632
Mahalia <m.henryrichards@arts.ac.uk>
2026-01-02 22:06:30 +0000

    add media panel and fixed browser style conflict

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ $(window).on('load', function () {
   $('.popupCloseButton').click(function(){
      $('.hover_bg').removeClass('show');
   });
+
+  // media panel
+  $('.media-toggle').on('click', function(e){
+    e.stopPropagation();
+    var $btn = $(this);
+    var $panel = $btn.siblings('.media-panel');
+    var open = $panel.hasClass('show');
+    $panel.toggleClass('show', !open).attr('hidden', open);
+    $btn.attr('aria-expanded', String(!open));
+  });
+
+  // stop clicks inside panel from closing it
+  $('.media-panel').on('click', function(e){ e.stopPropagation(); });
+
+  // close panel when clicking elsewhere
+  $(document).on('click', function(){
+    $('.media-panel').removeClass('show').attr('hidden', true);
+    $('.media-toggle').attr('aria-expanded', 'false');
+  });
 });
